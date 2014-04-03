ee5de9d4bd9ff8bff0a7fdbbd99534e9273ba087
Daniel Schmidt <dschmidt@weluse.de>
2014-04-03 21:33:49 +0200

    added raw tests

--- spec/number_fetcher_spec.rb
+++ spec/number_fetcher_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 describe NumberFetcher do
-  pending "write it"
+  describe "results" do
+    describe "large tournaments" do
+      it "should have the right date"
+      it "should have the right address"
+      it "should have the right kind"
+    end
+    describe "small tournaments" do
+      it "should have the right date"
+      it "should have the right address"
+      it "should have the right kind"
+    end
+  end
+
+  describe "options" do
+    it "should not take the cache if rerun is passed"
+    it "should update the cache if rerun is passed"
+    it "should not take cached if cached isn't passed"
+    it "should take the cached if cached is"
+  end
 end
